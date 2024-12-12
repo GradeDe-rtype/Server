dd83f9def8c91f44360f162a85147051c1b03d2b
Aeliondw <aeliondw@gmail.com>
2024-12-13 00:20:45 +0100

    add(game): add Timer class

---
+++ src/Game/Timer/Timer.cpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Timer class
+** Caroline Boilly @Aeliondw
+*/
+
+#include <Timer.hpp>
+
+
+namespace Server
+{
+    Timer::Timer(int intervalMs) : _interval(intervalMs)
+    {
+        _startTime = std::chrono::steady_clock::now();
+    }
+
+    void Timer::reset()
+    {
+        _startTime = std::chrono::steady_clock::now();
+    }
+
+    bool Timer::hasElapsed() const
+    {
+        auto now = std::chrono::steady_clock::now();
+        return std::chrono::duration_cast<std::chrono::milliseconds>(now - _startTime) >= _interval;
+    }
+
+    int Timer::timeLeft() const
+    {
+        auto now = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - _startTime);
+        return std::max(0, static_cast<int>(_interval.count() - elapsed.count()));
+    }
+}
---
+++ src/Game/Timer/Timer.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Timer class
+** Caroline Boilly @Aeliondw
+*/
+
+#ifndef TIMER_HPP
+    #define TIMER_HPP
+
+#include <RType.hpp>
+
+class Timer
+{
+private:
+    std::chrono::steady_clock::time_point _startTime;
+    std::chrono::milliseconds _interval;
+
+public:
+    Timer(int intervalMs);
+    ~Timer() = default;
+
+    void reset();
+    bool hasElapsed() const;
+    int timeLeft() const;
+};
+
+#endif // TIMER_HPP
